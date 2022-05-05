fb485de0f23cc9153596e96d65129794a2c219c3
VIJAYKUMAR ALAGIRISAMY (Europe - iDEAS-DX) <vi20279003@wipro.com>
2022-05-05 12:17:04 +0530

    05/May commit2 removed first import

--- summary/views.py
+++ summary/views.py
@@ -1,7 +1,4 @@
-from asyncio.windows_events import NULL
 from dataclasses import replace
-import imp
-from ssl import AlertDescription
 import uuid
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
@@ -65,7 +62,7 @@ def summary(request):
                 prompt1 = ""
                 prompt1 = '\n'.join(fullText)
             elif file_type in FILE_TYPES_IMG:
-                pytesseract.pytesseract.tesseract_cmd = 'C:/Users/VI20279003/AppData\Local/Programs/Tesseract-OCR/tesseract.exe'
+                pytesseract.pytesseract.tesseract_cmd = 'C:/Users/VI20279003/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
                 im = Image.open(myFile)
                 prompt1 = pytesseract.image_to_string(im)
             elif file_type not in FILE_TYPES and file_type not in FILE_TYPES_IMG and prompt1=="" and url=="":
